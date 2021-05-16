758a1da1b9b18502006b15372c5b44d51b1d0913
vanessa <vcollazos@hotmail.com>
2021-05-16 02:21:11 +0200

    first commit

---
+++ consoleApplication5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!!!");
+            int num1= 5;
+            int numero2= 6;
+            double numero3= 2.57;
+            double suma;
+            suma = num1+numero3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El númer introducido es: "+valor1);
+            inst valorint = Convert.ToInt32(valor1);
+            Console.ReadKey();
+        }
+    }
+}
